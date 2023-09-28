19d7adf36fbee6e5bc6e2e1f43b33bb34341fe83
Coral <cnight18@hotmail.com>
2023-09-28 09:36:12 -0300

    so many errors lol , fixed index missing express

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-// mongo 
+//  modules
 const mongoose = require('mongoose');
+const express = require('express');
 require('dotenv').config();
 
+// fforgot to add express whooopppss
+const app = express();
 const PORT = process.env.PORT || 3000;
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// import your route 
+// routes
 const userRoutes = require('./src/routes/user-routes');
 const thoughtRoutes = require('./src/routes/thought-routes');
 
-// use the routes
 app.use('/api/users', userRoutes);
 app.use('/api/thoughts', thoughtRoutes);
 
+// Add other routes here if needed ****
 
-// add other routes here if needed ****
-
-// mongo database here 
+// mongo  URI
 const dbURI = 'mongodb://localhost:27017/SocialNetwork';
 
-
+// connect to the Mongo
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
@@ -37,36 +39,31 @@ mongoose
     console.error('Error connecting to MongoDB:', error);
   });
 
-  // db connections 
-  mongoose.connection.on('connected', () => {
-    console.log('Mongoose connected to DB');
+//  mongo connection here
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+// close app
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through app termination');
+    process.exit(0);
   });
-  
-  mongoose.connection.on('error', (err) => {
-    console.error('Mongoose connection error:', err);
-  });
-  
-  mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose disconnected');
-  });
+});
 
-
-
-  process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-      console.log('Mongoose disconnected through app termination');
-      process.exit(0);
-    });
-  });
-
-
-
-// for fun
-console.log(
-    (`
+// for fun 
+console.log(`
      ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ 
     ||S |||o |||c |||i |||a |||l |||- |||N |||e |||t |||w |||o |||r |||k |||- |||A |||P |||I ||
     ||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__||
-    |/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
-  `)
-  );
+    |/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|
+`);
