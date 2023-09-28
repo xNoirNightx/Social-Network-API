f2cce71d135df5b6b1e5a1b39b10e21c6f053fc8
Coral <cnight18@hotmail.com>
2023-09-28 09:24:21 -0300

    connecting to mongo and set up insomnia

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+// mongo 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 // import your route 
 const userRoutes = require('./src/routes/user-routes');
 const thoughtRoutes = require('./src/routes/thought-routes');
@@ -8,6 +18,46 @@ app.use('/api/thoughts', thoughtRoutes);
 
 
 // add other routes here if needed ****
+
+// mongo database here 
+const dbURI = 'mongodb://localhost:27017/SocialNetwork';
+
+
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+  // db connections 
+  mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to DB');
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.error('Mongoose connection error:', err);
+  });
+  
+  mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+  });
+
+
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+      console.log('Mongoose disconnected through app termination');
+      process.exit(0);
+    });
+  });
 
 
 
